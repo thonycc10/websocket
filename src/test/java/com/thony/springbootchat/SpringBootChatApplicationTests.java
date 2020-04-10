50c89c4b30f231e2a081f43b0cd7fe39f845c3c6
Anthony Carrasco <thony10.rc.c@gmail.com>
2020-04-09 23:00:07 -0500

    creando proyecto

---
+++ src/test/java/com/thony/springbootchat/SpringBootChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thony.springbootchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
