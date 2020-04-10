b213b9e47afaaec8209a19d1c91f110772f95671
Anthony Carrasco <thony10.rc.c@gmail.com>
2020-04-10 01:04:17 -0500

    configuracion mensaje usuario nuevo, mensaje escribviendo

--- src/main/java/com/thony/springbootchat/controllers/ChatController.java
+++ src/main/java/com/thony/springbootchat/controllers/ChatController.java
@@ -6,16 +6,29 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import java.util.Date;
+import java.util.Random;
 
 @Controller
 public class ChatController {
+
+    private String[] colores = {"red", "yellow", "green", "magenta", "purple", "orange"};
 
      @MessageMapping("/mensaje")
      @SendTo("/chat/mensaje")
     public Mensaje reciberMensaje(Mensaje mens) {
         mens.setFecha(new Date().getTime());
-        mens.setTexto("Recibido por el broker: " + mens.getTexto());
+
+        if (mens.getTipo().equalsIgnoreCase("NUEVO_USUARIO")){
+            mens.setColor(colores[new Random().nextInt(colores.length)]);
+            mens.setTexto("nuevo usuario");
+        }
         return  mens;
      }
 
+     @MessageMapping("/escribiendo")
+    @SendTo("/chat/escribiendo")
+    public String estaEscribiendo(String username) {
+         return username.concat(" está escribiendo...");
+
+     }
 }
--- src/main/java/com/thony/springbootchat/models/documents/Mensaje.java
+++ src/main/java/com/thony/springbootchat/models/documents/Mensaje.java
@@ -6,6 +6,33 @@ public class Mensaje implements Serializable {
 
     private String texto;
     private Long fecha;
+    private String username;
+    private String tipo;
+    private String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 
     public String getTexto() {
         return texto;
